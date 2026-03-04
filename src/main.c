25f6bda3ffab4fa3d1589a86c2234c2a4f63a3ad
Pei-Han Jiang <peihan945@gmail.com>
2026-03-04 21:56:34 +0100

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -15,8 +15,21 @@ int main(int argc, char *argv[])
     fgets(cmd, 100, stdin);
     // fgets will include the '\n' symbol in the end
     cmd[strlen(cmd) - 1] = '\0';
-    if(strcmp(cmd, "exit") == 0) break;
-    printf("%s: command not found\n", cmd);
+
+    if (strcmp(cmd, "exit") == 0){
+      break;
+    }
+    else if(strncmp(cmd, "echo", 4) == 0){
+      printf(" \n");
+    }
+    else if (strncmp(cmd, "echo ", 5) == 0)
+    {
+      printf("%s", cmd + 5);
+      printf("\n");
+    }
+    else{
+      printf("%s: command not found\n", cmd);
+    }
   }
 
   return 0;
