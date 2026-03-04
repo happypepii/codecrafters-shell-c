e6e0e2fdbe700e90574b5d0bfcaaabc55b37196f
Pei-Han Jiang <peihan945@gmail.com>
2026-03-04 19:54:39 +0100

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -5,8 +5,12 @@ int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
+  char cmd[20];
   // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
+  scanf("%s", cmd);
+
+  printf("%s: command not found", cmd);
 
   return 0;
 }
