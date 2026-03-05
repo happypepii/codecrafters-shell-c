5be966ac642b3e636478b16c9a070cc1737600cc
Pei-Han Jiang <peihan945@gmail.com>
2026-03-05 14:07:18 +0100

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -4,6 +4,9 @@
 
 int main(int argc, char *argv[])
 {
+  char *builtinCmd[] = {"echo", "type", "exit"};
+  int length = 3;
+
   while (1)
   {
     // Flush after every printf
@@ -16,15 +19,31 @@ int main(int argc, char *argv[])
     // fgets will include the '\n' symbol in the end
     cmd[strlen(cmd) - 1] = '\0';
 
-    if (strcmp(cmd, "exit") == 0){
+    if (strcmp(cmd, "exit") == 0)
+    {
       break;
+    }
+    else if (strncmp(cmd, "type ", 5) == 0)
+    {
+      for (int i = 0; i < length; i++)
+      {
+        if (strcmp(builtinCmd[i], cmd + 5) == 0)
+        {
+          printf("%s is a shell builtins", cmd + 5);
+        }
+        else
+        {
+          printf("%s: not found", cmd + 5);
+        }
+      }
     }
     else if (strncmp(cmd, "echo ", 5) == 0)
     {
       printf("%s", cmd + 5);
       printf("\n");
     }
-    else{
+    else
+    {
       printf("%s: command not found\n", cmd);
     }
   }
