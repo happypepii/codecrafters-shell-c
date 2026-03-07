65324d7afd17d0d75f1e758dd20e0d6efea15baf
Pei-Han Jiang <peihan945@gmail.com>
2026-03-07 09:55:03 +0100

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 
-char* isExcutable(char *arg);
+char *isExecutable(char *arg);
 
 int main(int argc, char *argv[])
 {
@@ -38,14 +38,46 @@ int main(int argc, char *argv[])
       }
       else
       {
-        char* full_path = isExcutable(arg);
-        if(full_path != NULL){
+        char *full_path = isExecutable(arg);
+        if (full_path != NULL)
+        {
           printf("%s is %s\n", arg, full_path);
           free(full_path);
-        } else{
+        }
+        else
+        {
           printf("%s: not found\n", arg);
         }
       }
+    }
+    else if (isExecutable(cmd) != NULL)
+    {
+      // an executable program
+      pid_t pid = fork();
+      if (pid == -1)
+        perror("fork failed");
+      else if (pid == 0)
+      {
+        char *my_args[64];
+        int i = 0;
+
+        char *token = strtok(arg, " ");
+        while (token != NULL && i < 63)
+        {
+          my_args[i] = token;
+          i++;
+          token = strtok(arg, " ");
+        }
+        my_args[i] = NULL; // the last parameter for execvp has to be NULL
+        execvp(isExecutable(cmd), my_args); // if child process
+
+        // if execvp succeed, this will never be called.
+        perror("execv failed"); 
+        exit(1);
+      }
+      else
+        wait(NULL); // if parent process, wait for child process before proceeding.
+      // printf(".%s %s\n", isExecutable(cmd), arg);
     }
     else
     {
@@ -56,14 +88,15 @@ int main(int argc, char *argv[])
   return 0;
 }
 
-char* isExcutable(char *arg)
+char *isExecutable(char *arg)
 {
   char *path_env = getenv("PATH");
-  if (!path_env) return NULL;
+  if (!path_env)
+    return NULL;
 
   char *path_copy = strdup(path_env); // duplicate the path in case of modification
   char *dir = strtok(path_copy, ":");
-  char temp_path[1024]; 
+  char temp_path[1024];
   char *result = NULL;
 
   while (dir != NULL)
